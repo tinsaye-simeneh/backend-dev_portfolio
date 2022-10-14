150047caf39c08b3458c61c2c4dffec87692cd47
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-14 13:40:39 +0300

    columns created

--- src/componenets/ContactUs.js
+++ src/componenets/ContactUs.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const ContactUs = () => {
   return (
-    <>
-    </>
+    <div className="container bg-primary p-5" style={{marginTop:'7%'}}>
+     <div className="row">
+      <div className="col-md-6">
+       <h1 className="">Lets Chat <br/>Tell me about <br/> your project</h1>
+        </div>
+        <div className="col-md-6 bg-secondary">
+        col 2
+        </div>  
+        </div>
+    </div>
   )
 }
 
