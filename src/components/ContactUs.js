4c83d544837702875002b277c960f1c85c24d2f4
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-14 13:46:07 +0300

    first column text added

---
+++ src/components/ContactUs.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../index.css';
+
+const ContactUs = () => {
+  return (
+    <div className="container bg-dark border p-5" style={{marginTop:'7%'}}>
+     <div className="row">
+      <div className="col-md-6 border">
+       <h1 className="caros text-white p-5" style={{fontSize:'3.5rem', fontWeight:'bold'}}>Lets Chat <br/>Tell me about <br/> your project</h1>
+        </div>
+        <div className="col-md-6 border">
+        col 2
+        </div>  
+        </div>
+    </div>
+  )
+}
+
+export default ContactUs
