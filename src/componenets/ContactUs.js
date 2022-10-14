8376f4c5acafb7865f7713b05be0e1e0e566aa5b
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-14 15:13:32 +0300

    first column final code

--- src/componenets/ContactUs.js
+++ src/componenets/ContactUs.js
@@ -1,13 +1,27 @@
 import React from "react";
 import '../index.css';
+import medium from '../assets/image/medium.svg';
+import linkedin from '../assets/image/linkedin.svg';
+import twitter from '../assets/image/twitter.svg';
+import dev from '../assets/image/dev.svg';
+import Mail from '../assets/image/email.svg';
+
+
 
 const ContactUs = () => {
   return (
-    <div className="container bg-black border p-5" style={{marginTop:'7%'}}>
+    <div className="container bg-black p-5" style={{marginTop:'7%'}}>
      <div className="row">
-      <div className="col-md-6 border">
+      <div className="col-md-6">
        <h1 className="caros text-white px-5 pt-5" style={{fontSize:'3.5rem', fontWeight:'bold'}}>Lets Chat <br/>Tell me about <br/> your project</h1>
         <p className="caros text-white px-5 pt-2" style={{fontSize:'1.5rem'}}>Lets create something better</p>
+       <div className="row mt-5">
+        <img src={Mail} alt="email" className="ms-5" style={{width:'12%'}}/>
+        <img src={linkedin} alt="linkedin" style={{width:'12%'}}/>
+        <img src={twitter} alt="twitter" style={{width:'12%'}}/>
+        <img src={medium} alt="medium" style={{width:'12%'}}/>
+        <img src={dev} alt="dev" style={{width:'12%'}}/>
+        </div>
         </div>
         <div className="col-md-6 border">
         col 2
