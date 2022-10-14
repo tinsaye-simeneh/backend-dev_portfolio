13be19b07cb756a975b48d67be68759fbc69a2cc
Bereket kassa <bereketkassa3@gmail.com>
2022-10-14 11:11:34 +0300

    my new commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './componenets/Navbar';
 function App() {
   return (
-    <h2>portfolio project</h2>
+   <Navbar/>
   );
 }
 
