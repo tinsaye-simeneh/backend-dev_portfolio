852fd607eb19c3d07bad99865d9b9a74b0593e3b
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-14 16:01:53 +0300

    some minor editing on the master branch

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Navbar from './components/Navbar';
 import ContactUs from './components/ContactUs';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
