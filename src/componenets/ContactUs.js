0b0c3ec19291a3ec43373fda7febde5677a1397a
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-14 13:59:35 +0300

    first column second text added

--- src/componenets/ContactUs.js
+++ src/componenets/ContactUs.js
@@ -3,10 +3,11 @@ import '../index.css';
 
 const ContactUs = () => {
   return (
-    <div className="container bg-dark border p-5" style={{marginTop:'7%'}}>
+    <div className="container bg-black border p-5" style={{marginTop:'7%'}}>
      <div className="row">
       <div className="col-md-6 border">
-       <h1 className="caros text-white p-5" style={{fontSize:'3.5rem', fontWeight:'bold'}}>Lets Chat <br/>Tell me about <br/> your project</h1>
+       <h1 className="caros text-white px-5 pt-5" style={{fontSize:'3.5rem', fontWeight:'bold'}}>Lets Chat <br/>Tell me about <br/> your project</h1>
+        <p className="caros text-white px-5 pt-2" style={{fontSize:'1.5rem'}}>Lets create something better</p>
         </div>
         <div className="col-md-6 border">
         col 2
