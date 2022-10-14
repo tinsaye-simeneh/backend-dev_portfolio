0bbf24871a4cdcb4ecc9019b9b8eac67d0d30e21
Bereket kassa <bereketkassa3@gmail.com>
2022-10-14 11:34:03 +0300

    navbar completed

--- src/componenets/Navbar.js
+++ src/componenets/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "../logo.png"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
@@ -8,7 +10,7 @@ const Navbar = () => {
 
   <a className="navbar-brand text-white"><img className="logo" src={logo} alt="logo..."/>Backer Devis</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+ <FontAwesomeIcon icon={faBars} style={{ color: "#fff"}}/>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
