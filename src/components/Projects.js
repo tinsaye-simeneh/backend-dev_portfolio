49de73ffb13c8204b78ca4e05e012d23d419b422
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-15 20:21:27 +0300

    second row added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,19 @@
 import React from "react";
-import "./index.css";
+import "../index.css";
 
 const Projects = () => {
   return (
-    <div className="container mt-5">
+    <div className="container-fluid mt-5 bg-black">
       <div className="row">
         <div className="col-12">
-          <h1 className="text-center caros">SOME OF MY RECENT PROJECTS</h1>
+          <h4 className="text-center text-white mt-3 caros">SOME OF MY RECENT PROJECTS</h4>
         </div>
       </div>
-      <div className="row">yes</div>
+      <div className="row bg-white">
+        <div className="col-4 ms-5 text-white bg-primary">
+           he
+                        </div>
+      </div>
     </div>
   );
 };
