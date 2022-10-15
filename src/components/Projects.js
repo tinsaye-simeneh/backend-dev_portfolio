6d0b831fa76050f1d9cd58a144f265ea777894b3
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-15 20:32:06 +0300

    first image added to the col

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "../index.css";
+import dashboard from "../assets/images/dashboard.png";
+import ma from "../assets/images/ma.png";
 
 const Projects = () => {
   return (
     <div className="container-fluid mt-5 bg-black">
       <div className="row">
         <div className="col-12">
-          <h4 className="text-center text-white mt-3 caros">SOME OF MY RECENT PROJECTS</h4>
+          <h4 className="text-center text-white mt-3 caros">
+            SOME OF MY RECENT PROJECTS
+          </h4>
         </div>
       </div>
-      <div className="row bg-white">
-        <div className="col-4 ms-5 text-white bg-primary">
-           he
-                        </div>
+      <div className="row border">
+        <div className="col-4 ms-5 py-2 border text-white">
+            <div className="col-6 border bg-primary">
+                <img src={dashboard} alt="project" className="img-fluid" height='auto' />
+                </div>
+        </div>
       </div>
     </div>
   );
