b98ef159872b39384fdf55643eb700f475032d0f
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-15 19:41:46 +0300

    heading styles updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,17 @@
 import React from "react";
+import "./index.css";
 
 const Projects = () => {
-    return (
-        <div className="container mt-5">
-        <div className="row">
-            <div className="col-12">
-            <h1 className="text-center">Projects</h1>
-            </div>
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="text-center caros">SOME OF MY RECENT PROJECTS</h1>
         </div>
-        <div className="row">
-           yes
-        </div>
-        </div>
-    );
-    };
+      </div>
+      <div className="row">yes</div>
+    </div>
+  );
+};
 
 export default Projects;
