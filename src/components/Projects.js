7a767b7972f723bef743e54cf367523b1f388cff
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-15 21:14:23 +0300

    card details successfully fetched from the object

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,26 +3,7 @@ import "../index.css";
 import { Data } from "./data.js";
 
 const Projects = () => {
-  function ProjectCard() {
-      Data.map((data, key) => {
-        return (
-          <div className="col-4 py-4 text-white" style={{ marginLeft: "10%" }}>
-            <div className="row bg-white">
-              <div className="col-6 img-fluid">
-                <div key={key}>{data.img}</div>
-              </div>
-              <div className="col-6 text-center pt-4 pb-3">
-                <h5 className="text-black caros"> {data.title}</h5>
-                <p className="text-black caros"> {data.description}</p>
-                <span className="text-secondary caros">
-                  Client: {data.client}{" "}
-                </span>
-              </div>
-            </div>
-          </div>
-        );
-      });
-  }
+  function ProjectCard() {}
 
   return (
     <div className="container-fluid mt-5 py-5 bg-black">
@@ -34,12 +15,30 @@ const Projects = () => {
         </div>
       </div>
       <div className="row mt-5">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {Data.map((data) => (
+          <div
+            className="col-4 py-4 text-white"
+            key={data.id}
+            style={{ marginLeft: "10%" }}
+          >
+            <div className="row bg-white">
+              <div className="col-6 img-fluid">
+                <img
+                  src={data.img.src}
+                  alt={data.img.alt}
+                  className="img-fluid"
+                />
+              </div>
+              <div className="col-6 text-center pt-4 pb-3">
+                <h5 className="text-black caros"> {data.title}</h5>
+                <p className="text-black caros"> {data.description}</p>
+                <span className="text-secondary caros">
+                  Client: {data.client}{" "}
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
