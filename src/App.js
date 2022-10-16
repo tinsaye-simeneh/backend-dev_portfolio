51c27a6ad133c1a6a53105cad2a61ab1fb8f3fb8
Bereket kassa <bereketkassa3@gmail.com>
2022-10-16 14:27:33 +0300

    home page, it needs editing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './componenets/Navbar';
+import Home from './componenets/Home';
 function App() {
   return (
-   <Navbar/>
+   <><Navbar /><Home /></>
   );
 }
 
