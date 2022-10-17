109e34eae73003b1a841744da1fe6bb0d3d33d6f
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-17 09:17:02 +0300

    Home file added to the master branch

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import Navbar from './components/Navbar';
 import ContactUs from './components/ContactUs';
 import Projects from './components/Projects';
+import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   <Navbar/>
-  <Projects/>
+  <Home/>
   </>
 );
  
