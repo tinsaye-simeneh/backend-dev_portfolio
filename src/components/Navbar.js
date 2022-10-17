c7ec2c5ae1435f76f799fb38eef77ee20ea7d1c5
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-17 10:13:36 +0300

    Navbar menu links centered

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,24 +3,24 @@ import logo from '../assets/images/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-black pt-3">
         <div className="container">
 
-  <a className="navbar-brand text-white"><img className="logo" src={logo} alt="logo..."/>Backer Devis</a>
+  <a className="navbar-brand text-white" href="./Home"><img className="logo" src={logo} alt="logo..."/>Backer Devis</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+  <div className="collapse navbar-collapse text-center" id="navbarSupportedContent">
     <ul className="navbar-nav ms-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+        <a className="nav-link" href="./Home">Home <span className="sr-only"></span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Portfolio</a>
+        <a className="nav-link" href="./Projects">Portfolio</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Contact Me</a>
+        <a className="nav-link" href="./ContactUs">Contact Me</a>
       </li>
        </ul>
   
