48d190bfda29194a1136ef7e68a7f56d4eac0f22
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-10-17 20:01:28 +0300

    footer bg color and text changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,24 +4,31 @@ import reaclate from "../assets/images/reaclate.png";
 
 const Footer = () => {
   return (
-    <div className="container-fluid bg-black py-5">
+    <div className="container-fluid bg-white py-3 border">
       <div className="row">
-        <div className="col-md-4 text-white">
+        <div className="col-md-4 text-black">
           © 2021. All Rights Reserved. Designed by
           <a href="#" className="caros text-decoration-none">
             {" "}
             uix Nature
           </a>
         </div>
-        <div className="col-md-4 text-center text-white">
-          Proudlt powered by
-          <a href="https://t.me/uixnature" className="text-white caros">
-            <img src={uix} alt="uix" className="img-fluid" />
+        <div className="col-md-6 text-center text-black">
+            <span className="px-2">
+          Proudly powered by
+    </span>
+          <a href="https://t.me/uixnature" className="text-black caros">
+            <img src={uix} alt="uix" className="img-fluid mb-2" />
           </a>
+          <span className="px-2">
           and 
-          <a href="https://github.com/Reaclate" className="text-white caros">
-            <img src={reaclate} alt="reaclate" className="img-fluid" />
+    </span>
+          <a href="https://github.com/Reaclate" className="text-black caros">
+            <img src={reaclate} alt="reaclate" className="img-fluid mb-2" />
           </a>
+          <span className="px-2">
+          collaborated with <a href="https://github.com/bekisha" className="text-black caros">Bekisha</a>
+          </span>
         </div>
       </div>
     </div>
